5bdfd46f55e3f7ef313a47acf880ac4886d9312d
fabioserrano-hub <fabioacs23@gmail.com>
2026-06-18 23:41:27 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { ToastProvider } from './components/ui'
 import { AuthProvider, useAuth } from './hooks/useAuth'
 
